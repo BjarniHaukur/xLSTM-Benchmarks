896d7bc982dabd2fc94630b43c382b9eced726c8
Bjarni Haukur <bjarnihaukur11@gmail.com>
2024-05-31 02:30:56 +0200

    cleaner

--- xlstm_benchmarks/models/rnn.py
+++ xlstm_benchmarks/models/rnn.py
@@ -7,7 +7,7 @@ class RNNCell(nn.Module):
     def __init__(self, input_size:int, hidden_size:int):
         super(RNNCell, self).__init__()
 
-        self.WI = nn.Parameter(torch.empty((hidden_size, input_size)))
+        self.WI = nn.Parameter(torch.empty((input_size, hidden_size)))
         self.BI = nn.Parameter(torch.empty(hidden_size))
         self.WH = nn.Parameter(torch.empty((hidden_size, hidden_size)))
         self.BH = nn.Parameter(torch.empty(hidden_size))
@@ -19,7 +19,7 @@ class RNNCell(nn.Module):
         nn.init.xavier_normal_(self.WH), nn.init.zeros_(self.BH)
 
     def forward(self, x:Tensor, h:Tensor)->Tensor:
-        return torch.tanh(x @ self.WI.T + self.BI + h @ self.WH.T + self.BH)
+        return torch.tanh(x @ self.WI + self.BI + h @ self.WH + self.BH)
 
 
 class RNN(nn.Module):
@@ -39,9 +39,8 @@ class RNN(nn.Module):
         output = []
         for t in range(L):
             for i, cell in enumerate(self.rnn_cells):
-                x_layer = x[:, t] if i == 0 else h_t[i - 1] # stacked layers receive the output of the previous layer
-                h_t[i] = cell(x_layer, h_t[i])
-                
+                h_t[i] = cell(x[:, t] if i==0 else h_t[i - 1], h_t[i]) # stacked layers receive the output of the previous layer
+            
             output.append(h_t[-1])
 
         return torch.stack(output, dim=1), torch.stack(h_t, dim=0)
@@ -57,16 +56,16 @@ if __name__ == "__main__":
     parser.add_argument("--input_size", type=int, default=32)
     parser.add_argument("--hidden_size", type=int, default=128)
     parser.add_argument("--n_layers", type=int, default=2)
-    parser.add_argument("--forward_tolerance", type=float, default=1e-10)
-    parser.add_argument("--backward_tolerance", type=float, default=1e-3)
+    parser.add_argument("--forward_tolerance", type=float, default=1e-16) # perfect match
+    parser.add_argument("--backward_tolerance", type=float, default=1e-4) # lossy match
     args = parser.parse_args()
     
     rnn = RNN(args.input_size, args.hidden_size, args.n_layers)
     torch_rnn = nn.RNN(args.input_size,  args.hidden_size, args.n_layers, nonlinearity="tanh", batch_first=True)
 
     for i, cell in enumerate(rnn.rnn_cells):
-        getattr(torch_rnn, "weight_ih_l" + str(i)).data.copy_(cell.WI.data)
-        getattr(torch_rnn, "weight_hh_l" + str(i)).data.copy_(cell.WH.data)
+        getattr(torch_rnn, "weight_ih_l" + str(i)).data.copy_(cell.WI.data.T)
+        getattr(torch_rnn, "weight_hh_l" + str(i)).data.copy_(cell.WH.data.T)
         getattr(torch_rnn, "bias_ih_l" + str(i)).data.copy_(cell.BI.data)
         getattr(torch_rnn, "bias_hh_l" + str(i)).data.copy_(cell.BH.data)
 
@@ -74,8 +73,8 @@ if __name__ == "__main__":
     out, h = rnn(x)
     out_torch, h_torch = torch_rnn(x)
 
-    np.testing.assert_allclose(out.detach().numpy(), out_torch.detach().numpy(), rtol=args.forward_tolerance)
-    np.testing.assert_allclose(h.detach().numpy(), h_torch.detach().numpy(), rtol=args.forward_tolerance)
+    np.testing.assert_allclose(out.detach().numpy(), out_torch.detach().numpy(), atol=args.forward_tolerance)
+    np.testing.assert_allclose(h.detach().numpy(), h_torch.detach().numpy(), atol=args.forward_tolerance)
 
     print(f"Forwards pass match with {args.forward_tolerance=}")
 
@@ -83,10 +82,10 @@ if __name__ == "__main__":
     out_torch.sum().backward()
 
     for i, cell in enumerate(rnn.rnn_cells):
-        np.testing.assert_allclose(cell.WI.grad.detach().numpy(), getattr(torch_rnn, "weight_ih_l" + str(i)).grad.detach().numpy(), rtol=args.backward_tolerance)
-        np.testing.assert_allclose(cell.WH.grad.detach().numpy(), getattr(torch_rnn, "weight_hh_l" + str(i)).grad.detach().numpy(), rtol=args.backward_tolerance)
-        np.testing.assert_allclose(cell.BI.grad.detach().numpy(), getattr(torch_rnn, "bias_ih_l" + str(i)).grad.detach().numpy(), rtol=args.backward_tolerance)
-        np.testing.assert_allclose(cell.BH.grad.detach().numpy(), getattr(torch_rnn, "bias_hh_l" + str(i)).grad.detach().numpy(), rtol=args.backward_tolerance)
+        np.testing.assert_allclose(cell.WI.grad.detach().numpy().T, getattr(torch_rnn, "weight_ih_l" + str(i)).grad.detach().numpy(), atol=args.backward_tolerance)
+        np.testing.assert_allclose(cell.WH.grad.detach().numpy().T, getattr(torch_rnn, "weight_hh_l" + str(i)).grad.detach().numpy(), atol=args.backward_tolerance)
+        np.testing.assert_allclose(cell.BI.grad.detach().numpy().T, getattr(torch_rnn, "bias_ih_l" + str(i)).grad.detach().numpy(), atol=args.backward_tolerance)
+        np.testing.assert_allclose(cell.BH.grad.detach().numpy().T, getattr(torch_rnn, "bias_hh_l" + str(i)).grad.detach().numpy(), atol=args.backward_tolerance)
 
     print(f"Backwards pass match with {args.backward_tolerance=}")
     
